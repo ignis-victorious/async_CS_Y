46b0cd86c16d803466685b345893151240f99f8e
Emmanuele Levi <ignisvictorious@gmail.com>
2026-02-21 18:09:59 +0000

    Same event loop, no real advantage in having async

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 # 
 #  Import LIBRARIES
+import asyncio
 import time
 #  Import FILES
 #  ______________________
 # 
 
-def fetch_data(param):
-    print (f"Do something with {param}...")
-    time.sleep(param)
-    print(f"Done with {param}")
-    return f"Result of {param} "
+async def fetch_data (param):
+    print(f"Do something with {param}...") 
+    await asyncio.sleep(param)
+    print(f"Done with {param}") 
+    return f"Result of {param}"
 
 
-def main():
-    result1 = fetch_data(1)
-    print("Fetch 1 fully completed")
-    result2 = fetch_data(2)
-    print("Fetch 2 fully completed")
+async def main():
+    task1 = fetch_data(1)  # Could be awaited directly
+    task2 = fetch_data(2)  # Could be awaited directly
+    result1 = await task1
+    print ("Task 1 fully completed")
+    result2 = await task2
+    print ("Task 2 fully completed") 
     return [result1, result2]
 
-results = main()
+
+
+t1 = time.perf_counter()  # Start the counter
+
+results = asyncio.run(main())
 print(results)
-print('Main completed')
+
+t2 = time.perf_counter ()  # Stop the counter
+print(f"Finished in {t2 - t1:.2f} seconds")
+
 
 
 
