2a2284d76c70ba1e7ec8f2502b1dd414a69a5fbf
Emmanuele Levi <ignisvictorious@gmail.com>
2026-02-21 18:24:50 +0000

    Concurrency in action

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 
 async def fetch_data (param):
     print(f"Do something with {param}...") 
-    await asyncio.sleep(param)
+    await asyncio.sleep(param) # Suspends current task till 'sleep' is completed 
     print(f"Done with {param}") 
     return f"Result of {param}"
 
@@ -16,7 +16,7 @@ async def fetch_data (param):
 async def main():
     task1 = asyncio.create_task(fetch_data(1))  
     task2 = asyncio.create_task(fetch_data(2.1) ) 
-    result1 = await task1
+    result1 = await task1  # Yield control to the event loop (main coroutine suspended till task1 completed)
     print ("Task 1 fully completed")
     result2 = await task2
     print ("Task 2 fully completed") 
